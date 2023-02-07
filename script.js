9d58248f6059389ed3e53c3851cca25b57286c82
David Ricketts <david.ricketts119@gmail.com>
2023-02-07 08:56:47 -0500

    Script p2

--- script.js
+++ script.js
@@ -47,5 +47,14 @@ const switchPlayer = function () {
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-    
+    if (playing) {
+        // 1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        //2. Display dice 
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+
+        // 3. Check for rolled 1 
+    }
 })
